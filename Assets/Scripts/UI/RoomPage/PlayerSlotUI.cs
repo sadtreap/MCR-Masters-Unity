b358700776f896f83d58b8ab5d40cbdffb46b6e6
Gamja <71741826+Gamja3288@users.noreply.github.com>
2025-05-03 12:31:55 +0900

    code로 조회 통일
    yea

--- Assets/Scripts/UI/RoomPage/PlayerSlotUI.cs
+++ Assets/Scripts/UI/RoomPage/PlayerSlotUI.cs
@@ -18,7 +18,7 @@ namespace MCRGame.UI
             nameText.gameObject.SetActive(true);
             readyIndicator.gameObject.SetActive(true);
             characterImage.gameObject.SetActive(true);                
-            characterImage.sprite = CharacterImageManager.Instance.get_character_sprite_by_code(info.current_character.name);
+            characterImage.sprite = CharacterImageManager.Instance.get_character_sprite_by_code(info.current_character.code);
             characterImage.color = new Color(255, 255, 255, 255);
             nameText.text = info.nickname + (info.uid == hostUid ? " (Host)" : "");
             SetReady(info.is_ready);
